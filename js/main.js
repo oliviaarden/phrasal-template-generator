30adc5cadf703561ed0787b4f62c3ad2d9fcc078
oliviaarden <mac99@SRMAC02.local>
2016-03-01 14:35:02 -0500

    getting through js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var intro = alert ('Let’s make a story together!');
+var colour = prompt ('A colour (orange, indigo, etc.)')
+var creature = prompt ('A creature (dragon, moth, etc.)')
+var adjective = prompt ('An adjective (beautiful, super, etc.)')
+var verb = prompt ('A past tense verb (ran, burped, etc.)')
+var story = prompt ('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning')
+
+var intro;
+var colour;
+var creature;
+var adjective;
+var verb;
+var story;
+
+var storyA = function () {
+  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
+};
+var storyB = function () {
+  document.write('Ameilia ' + verb + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + );
+};
+var storyC = function () {
+  document.write('Jackson chose his ' + adjective, + ' ' + colour + ' ' + creature + ' card and ' + verb + ' it to the table knowing he won.');
+};
